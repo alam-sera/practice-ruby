1b553078336ce57f157603a13412276732a7bb0f
Alex Lam <alam.seraph@gmail.com>
2022-11-29 11:04:12 -0800

    added items

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+item1 = {color: "blue", price: 8, shelf_life: "1 year"}
+item2 = {color: "black", price: 15, shelf_life: "5 year"}
+item3 = {color: "purple and pink", price: 30, shelf_life: "NA"}
+
+p item1
+p item2
+p item3
